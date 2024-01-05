0c53dc30de0d4a8fb71fe22b781be9f95584a9cf
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-05 23:27:19 +0900

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from './components/Main/Header';
+import { styled } from 'styled-components';
+
+function App() {
+  return <Header />;
+}
+
+export default App;

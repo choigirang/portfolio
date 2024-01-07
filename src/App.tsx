8a49fb8229a5582f64e0674aaed60ae56828d3a6
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-07 23:48:11 +0900

    style: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Header from './components/Main/Header';
 import { styled } from 'styled-components';
+import Main from './components/Main/Main';
 
 function App() {
-  return <Header />;
+  return (
+    <Container>
+      <Header />
+      <Main></Main>
+    </Container>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 200vh;
+`;

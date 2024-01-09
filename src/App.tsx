7452e7e6a244e6196cd798b1911646d6adecf9c1
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-09 22:46:12 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import Header from './components/Main/Header';
+import Header from './components/Header';
 import { styled } from 'styled-components';
 import Main from './components/Main/Main';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import About from './components/About/About';
 
 function App() {
   return (
     <Container>
-      <Header />
-      <Main></Main>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main></Main>} />
+          <Route path="/About" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }

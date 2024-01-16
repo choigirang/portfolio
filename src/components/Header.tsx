a34b22c2b12e1e0502b8f95dcb87efdfd85c9c8e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-16 22:43:36 +0900

    feat: components에 따른 router 변경
    새로고침 시 기본값 Home으로 유지되는 => 동적으로 변경 필요

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { HeaderScrollState } from '../type/mainType';
-import { matchPath, Link, useLocation } from 'react-router-dom';
-import { Tabs, Tab, useTheme, styled } from '@mui/material';
+import { HeaderScrollState, TabPropsType } from '../type/headerType';
+import { Link } from 'react-router-dom';
+import { Tabs, Tab, useTheme, styled as MuiStyled } from '@mui/material';
+import useRouteMatch from '../hooks/useRouteMatch';
 
 // 주소 목록
 const lists = ['Home', 'About', 'Skills', 'Experience'];
@@ -12,12 +13,14 @@ const lists = ['Home', 'About', 'Skills', 'Experience'];
  */
 export default function Index() {
   const [scroll, setScroll] = useState<boolean>(false);
+  const [activeSection, setActiveSection] = useState<string>('Home');
 
-  const routeMatch = useRouteMatch(['/Home', '/About', '/Skills', '/Experience']);
+  const routeMatch = useRouteMatch();
 
   const theme = useTheme();
 
-  const scrollHandler = (e: string) => {
+  // 페이지 전환
+  const pageScrollHandler = (e: string) => {
     document.getElementById(e)?.scrollIntoView({
       behavior: 'smooth',
     });
@@ -47,20 +50,47 @@ export default function Index() {
     };
   }, [scroll]);
 
+  // 화면에 들어오는 컴포넌트에 따른 주솟값 변경
+  useEffect(() => {
+    const scrollChangeRouter = () => {
+      const section = lists.find(list => {
+        const element = document.getElementById(list);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          return rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+        }
+        return false;
+      });
+
+      if (section && section !== activeSection) {
+        setActiveSection(section);
+        window.history.replaceState(null, '', `/${section}`);
+      }
+    };
+
+    window.addEventListener('scroll', scrollChangeRouter);
+
+    return () => {
+      window.removeEventListener('scroll', scrollChangeRouter);
+    };
+  }, [activeSection]);
+
+  console.log(activeSection);
+
   return (
     <Header $scroll={scroll} theme={theme}>
       <Logo></Logo>
       <Tabs
-        value={routeMatch}
+        value={'/' + activeSection}
         sx={{ '& .MuiTabs-indicator': { backgroundColor: 'transparent' }, '& .Mui-selected': { color: 'purple' } }}>
         {lists.map(list => (
-          <Tab
+          <TabList
             key={list}
             label={list.replace('/', '')}
             value={'/' + list}
             to={list}
             component={Link}
-            onClick={() => scrollHandler(list)}
+            onClick={() => pageScrollHandler(list)}
           />
         ))}
       </Tabs>
@@ -68,27 +98,7 @@ export default function Index() {
   );
 }
 
-/**
- *
- * @param patterns router의 주솟값
- * @returns useLoaction으로 현재 주솟값을 확인하여 mui Tab과 매칭
- */
-function useRouteMatch(patterns: readonly string[]) {
-  const { pathname } = useLocation();
-
-  // 주솟값 매칭
-  for (let i = 0; i < patterns.length; i += 1) {
-    const pattern = patterns[i];
-    const possibleMatch = matchPath(pattern, pathname);
-    if (possibleMatch !== null) {
-      return possibleMatch.pattern.path;
-    }
-  }
-
-  return null;
-}
-
-const Header = styled('div')<HeaderScrollState>(({ theme, $scroll }) => ({
+const Header = MuiStyled('div')<HeaderScrollState>(({ theme, $scroll }) => ({
   display: 'flex',
   position: 'fixed',
   top: 0,
@@ -103,16 +113,18 @@ const Header = styled('div')<HeaderScrollState>(({ theme, $scroll }) => ({
   transition: 'all 1s',
   zIndex: 1,
 
-  '.Mui-selected': {
-    color: theme.palette.mode === 'dark' ? '#ffb700' : '#000f1f',
-  },
-
   '.visited': {
     color: theme.palette.mode === 'dark' ? '#ffb700' : '#000f1f',
   },
 }));
 
-const Logo = styled('a')({
+const TabList = MuiStyled(Tab)<TabPropsType>(({ theme }) => ({
+  '.Mui-selected': {
+    color: theme.palette.mode === 'dark' ? '#ffb700' : '#000f1f',
+  },
+}));
+
+const Logo = MuiStyled('a')({
   width: '150px',
   height: '100%',
   backgroundColor: 'black',

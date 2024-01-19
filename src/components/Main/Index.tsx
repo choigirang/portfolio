dfadf1bb15871fbbbb2c9551ae57eef21292e218
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-19 20:13:01 +0900

    feat: main 구조

--- src/components/Main/Index.tsx
+++ src/components/Main/Index.tsx
@@ -1,12 +1,23 @@
-import { Container } from '@mui/material';
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import { styled as MuiStyled } from '@mui/material';
 
 export default function Index() {
-  return <Containter id="Home"></Containter>;
+  return (
+    <Containter id="Home">
+      <Profile></Profile>
+    </Containter>
+  );
 }
 
-const Containter = styled('main')({
+const Containter = MuiStyled('main')({
   width: '100%',
   height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const Profile = MuiStyled('div')({
+  width: 400,
+  height: 500,
+  backgroundColor: 'black',
 });

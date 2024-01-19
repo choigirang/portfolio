9f0cdb0ad0d6a9d430422751b31ada28a45b19fe
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-19 20:13:34 +0900

    delete trash code & add typography theme

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,45 +1,14 @@
 import { PaletteMode, createTheme } from '@mui/material';
 import Typography from './Typography';
-// const Palette = (): PaletteOptions => {
-//   return {
-//     primary: {
-//       main: '#FF6415',
-//       dark: '',
-//     },
-//     secondary: {
-//       main: '#E3BA8A',
-//       dark: '',
-//     },
-//     background: {
-//       default: '#FFFFFFF',
-//       paper: '',
-//     },
-//     error: {
-//       main: '#FF4545',
-//       dark: '',
-//     },
-//     info: {
-//       main: '#1A1A1A',
-//       dark: '',
-//     },
-//     grey: {
-//       50: '#FCFCFC',
-//       100: '#EFEFEF',
-//       200: '#DFDFDF',
-//       300: '#B7B7B7',
-//       400: '#949494',
-//       500: '#777777',
-//       600: '#555555',
-//       700: '#3F3F3F',
-//       800: '#2A2A2A',
-//       900: '#1A1A1A',
-//     },
-//   };
-// };
-// export default Palette;
 
 const createCustomTheme = (mode: PaletteMode) =>
   createTheme({
+    typography: {
+      h1: {
+        fontSize: 30,
+        fontWeight: 700,
+      },
+    },
     palette: {
       mode,
       common: {

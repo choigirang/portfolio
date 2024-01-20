2fd99d4a07c7892e5c8457bc0bc08892a7b70042
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-20 23:11:29 +0900

    feat: mail onClick, move contact component & style(darkMode && openMenu)

---
+++ src/components/sections/Footer/Index.tsx
@@ -0,0 +1,203 @@
+import { styled as MuiStyled, IconButton } from '@mui/material';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import ListIcon from '@mui/icons-material/List';
+import React, { useState } from 'react';
+import DarkModeSwitch from './DarkModeSwitch';
+import { FaInstagram } from 'react-icons/fa';
+import { MenuOpenType } from '../../../type/footerType';
+import { FaGithub } from 'react-icons/fa';
+import { IoMail } from 'react-icons/io5';
+
+export default function Footer() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const topScroll = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const openMenuHandler = () => {
+    setOpenMenu(prev => !prev);
+  };
+
+  const openMyInfo = (url: string) => {
+    if (url === 'insta') return window.open('https://www.instagram.com/choi_girang/', '_blank');
+    if (url === 'github') return window.open('https://github.com/choigirang');
+  };
+
+  const moveContact = () => {
+    document.getElementById('Contact')?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <Container>
+      <UpIconBtn onClick={topScroll}>
+        <UpIconStyling fontSize="large" />
+      </UpIconBtn>
+      <Menu>
+        <ListIconBtn $openMenu={openMenu} onClick={openMenuHandler}>
+          <ListIconStyle $openMenu={openMenu} fontSize="large" />
+        </ListIconBtn>
+        <DarkModeSwitch $openMenu={openMenu} />
+        <InstaIconBtn size={30} $openMenu={openMenu} onClick={() => openMyInfo('insta')} />
+        <GitIconBtn size={30} $openMenu={openMenu} onClick={() => openMyInfo('github')} />
+        <MailIconBox $openMenu={openMenu} onClick={() => moveContact()}>
+          <MailIconBtn size={30} $openMenu={openMenu}></MailIconBtn>
+        </MailIconBox>
+      </Menu>
+    </Container>
+  );
+}
+
+const Container = MuiStyled('footer')({
+  position: 'fixed',
+  display: 'flex',
+  bottom: 0,
+  justifyContent: 'end',
+  alignItems: 'center',
+  width: '100vw',
+  height: '10%',
+  padding: '0 calc((100vw - 1280px) / 2);',
+  transition: 'all .5s ease !important',
+});
+
+const UpIconBtn = MuiStyled(IconButton)(({ theme }) => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%,-50%)',
+}));
+
+const UpIconStyling = MuiStyled(ArrowCircleUpIcon)(({ theme }) => ({
+  color: '#828282f !important',
+}));
+
+const Menu = MuiStyled('div')({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column-reverse',
+  justifyContent: 'center',
+  transition: 'all 2s',
+});
+
+const ListIconBtn = MuiStyled(IconButton)<MenuOpenType>(({ $openMenu, theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  borderRadius: '50%',
+  zIndex: 999,
+
+  '&:hover': {
+    backgroundColor: theme.palette.secondary.main,
+  },
+
+  ...(theme.palette.mode === 'dark' && {
+    backgroundColor: theme.palette.primary.dark,
+
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.dark,
+    },
+  }),
+
+  ...($openMenu && {
+    backgroundColor: 'none',
+  }),
+}));
+
+const ListIconStyle = MuiStyled(ListIcon)<MenuOpenType>(({ $openMenu, theme }) => ({
+  color: 'white',
+
+  ...($openMenu && {
+    color: theme.palette.primary.main,
+  }),
+
+  ...(theme.palette.mode === 'dark' &&
+    $openMenu && {
+      color: theme.palette.secondary.dark,
+    }),
+
+  '&:hover': {
+    color: 'white',
+  },
+}));
+
+const InstaIconBtn = MuiStyled(FaInstagram)<MenuOpenType>(({ $openMenu, theme }) => ({
+  position: 'absolute',
+  bottom: 130,
+  left: '50%',
+  transform: 'translateX(-50%)',
+  transition: 'all .5s ease',
+  zIndex: 100,
+  color: theme.palette.primary.main,
+
+  '&:hover': {
+    cursor: 'pointer',
+  },
+
+  ...(!$openMenu && {
+    bottom: 0,
+    opacity: 0,
+  }),
+
+  ...(theme.palette.mode === 'dark' && {
+    color: theme.palette.primary.dark,
+  }),
+}));
+
+const GitIconBtn = MuiStyled(FaGithub)<MenuOpenType>(({ $openMenu, theme }) => ({
+  position: 'absolute',
+  bottom: 180,
+  left: '50%',
+  transform: 'translateX(-50%)',
+  transition: 'all .5s ease',
+  zIndex: 100,
+  color: theme.palette.primary.main,
+
+  '&:hover': {
+    cursor: 'pointer',
+  },
+
+  ...(!$openMenu && {
+    bottom: 0,
+    opacity: 0,
+  }),
+
+  ...(theme.palette.mode === 'dark' && {
+    color: theme.palette.primary.dark,
+  }),
+}));
+
+const MailIconBox = MuiStyled('div')<MenuOpenType>(({ $openMenu, theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  position: 'absolute',
+  bottom: 230,
+  left: '50%',
+  transform: 'translateX(-50%)',
+  transition: 'all .5s ease',
+
+  '&:hover': {
+    cursor: 'pointer',
+  },
+
+  ...(!$openMenu && {
+    bottom: 0,
+    opacity: 0,
+  }),
+}));
+
+const MailIconBtn = MuiStyled(IoMail)<MenuOpenType>(({ $openMenu, theme }) => ({
+  color: theme.palette.primary.main,
+  transition: 'all .5s ease',
+
+  ...(!$openMenu && {
+    bottom: 0,
+    opacity: 0,
+  }),
+
+  ...(theme.palette.mode === 'dark' && {
+    color: theme.palette.primary.dark,
+  }),
+}));

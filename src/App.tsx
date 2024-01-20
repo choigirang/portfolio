40bcfb373daccd5f9acbc57262d2a3c3e881f4ec
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-20 17:49:16 +0900

    feat: useColorMode context 분리

--- src/App.tsx
+++ src/App.tsx
@@ -3,46 +3,30 @@ import Header from './components/Header';
 import Main from './components/Main/Index';
 import Skills from './components/Skills/Index';
 import Experience from './components/Experience/Index';
+import Footer from './components/Footer/Index';
 import { BrowserRouter } from 'react-router-dom';
 import About from './components/About/Index';
-import { PaletteMode, ThemeProvider, styled as MuiStyled } from '@mui/material';
-import { StyledEngineProvider } from '@mui/styled-engine-sc';
-import DarkModeSwitch from './components/DarkModeSwitch';
+import { ThemeProvider, styled as MuiStyled } from '@mui/material';
 import createCustomTheme from './theme/palette';
-
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+import { useColorMode } from './hooks/useColorMode';
 
 function App() {
-  const [mode, setMode] = React.useState<PaletteMode>('light');
-  const colorMode = React.useMemo(
-    () => ({
-      // The dark mode switch would invoke this method
-      toggleColorMode: () => {
-        setMode((prevMode: PaletteMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
-
+  const { mode } = useColorMode();
   const theme = useMemo(() => createCustomTheme(mode), [mode]);
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <StyledEngineProvider>
-          <Container>
-            <BrowserRouter>
-              <Header />
-              <Main />
-              <About />
-              <Skills />
-              <Experience />
-            </BrowserRouter>
-          </Container>
-        </StyledEngineProvider>
-        <DarkModeSwitch colorMode={colorMode} />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <BrowserRouter>
+          <Header />
+          <Main />
+          <About />
+          <Skills />
+          <Experience />
+          <Footer />
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
   );
 }
 

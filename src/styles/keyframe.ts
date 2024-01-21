48868c251d38d11ee9fcea4a77a3a6e3d0d3197a
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-21 23:38:08 +0900

    feat: add plane animation keyframes

--- src/styles/keyframe.ts
+++ src/styles/keyframe.ts
@@ -10,3 +10,33 @@ export const textEaseEffect = keyframes`
     opacity: 1;
   }
 `;
+
+export const planeContainerAni = keyframes`
+  0% {
+    transform: translateY(0px);
+  }
+  100% {
+    transform: translateY(100px);
+  }
+`;
+
+export const planeAni = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  40% {
+    transform: rotate(15deg);
+  }
+  50% {
+    transform: rotate(15deg);
+  }
+  60% {
+    transform: rotate(-10deg);
+  }
+  70% {
+    transform: rotate(-10deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+`;

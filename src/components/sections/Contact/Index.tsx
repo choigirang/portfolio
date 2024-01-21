e292066e179ee8fac77368b7f52271a071e77e3c
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-21 23:37:20 +0900

    feat: emailjs를 이용한 contact 이메일 전송

--- src/components/sections/Contact/Index.tsx
+++ src/components/sections/Contact/Index.tsx
@@ -1,11 +1,104 @@
-import { styled as MuiStyled } from '@mui/material';
-import React from 'react';
+import { Button, styled as MuiStyled, TextField } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import React, { useRef } from 'react';
+import Plane from './Plane';
+import { ContactType } from '../../../type/contactType';
+import emailjs from '@emailjs/browser';
+
+const contact: ContactType = {
+  Name: 'text',
+  Mail: 'email',
+  Content: 'text',
+};
 
 export default function Contact() {
-  return <Container id="Contact">Contact</Container>;
+  const PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_KEY;
+  const SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+  const TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current) return console.log('form null');
+
+    if (!SERVICE_ID || !TEMPLATE_ID) return console.log('send mail func has wrong key', SERVICE_ID, TEMPLATE_ID);
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+      result => {
+        console.log(result.text);
+      },
+      error => {
+        console.log(error.text);
+      },
+    );
+  };
+
+  return (
+    <Container id="Contact">
+      <Plane />
+      <Board ref={form} onSubmit={sendEmail}>
+        {(Object.keys(contact) as (keyof ContactType)[]).map(each => (
+          <TextInput
+            key={each}
+            name={each}
+            label={each}
+            id={each}
+            type={contact[each]}
+            multiline
+            rows={each === 'Content' ? 5 : undefined}
+          />
+        ))}
+        <Button startIcon={<SendIcon />} variant="contained" type="submit">
+          SEND
+        </Button>
+      </Board>
+    </Container>
+  );
 }
 
-const Container = MuiStyled('div')({
+const Container = MuiStyled('div')(({ theme }) => ({
   width: '100vw',
   height: '100vh',
-});
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  transition: 'all .5s ease-in-out',
+
+  ...(theme.palette.mode === 'dark' && {
+    backgroundColor: theme.palette.primary.dark,
+  }),
+}));
+
+const Board = MuiStyled('form')(({ theme }) => ({
+  width: '40%',
+  height: '60%',
+  maxWidth: 600,
+  maxHeight: 700,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '20px',
+  padding: '0 5%',
+  paddingTop: '10%',
+  borderRadius: 50,
+  backgroundColor: theme.palette.secondary.main,
+  boxShadow: 'inset 0 5px 5px -5px #333, inset 0 -5px 5px -5px #333',
+  transition: 'all .5s ease-in-out',
+
+  ...(theme.palette.mode === 'dark' && {
+    backgroundColor: theme.palette.common.white,
+  }),
+}));
+
+const TextInput = MuiStyled(TextField)(({ theme }) => ({
+  outline: 'none',
+  backgroundColor: 'white',
+
+  '& textarea, & div': {
+    color: 'black',
+    outline: 'none',
+    border: 'none',
+  },
+}));

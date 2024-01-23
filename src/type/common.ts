ea8dd3069ec209b8d08c512a09f229f07cad2835
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-23 20:14:30 +0900

    feat: turtle ani 에서 사용할 props type

--- src/type/common.ts
+++ src/type/common.ts
@@ -2,3 +2,8 @@ export type ColorModeContextType = {
   mode: 'light' | 'dark';
   toggleColorMode: () => void;
 };
+
+export type TurtleAnimationProps = {
+  $resetPath?: string;
+  $turtleX?: number;
+};

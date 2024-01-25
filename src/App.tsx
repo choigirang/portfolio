e366a9d021242e8b086af1e00399735858ef0c68
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-25 15:13:19 +0900

    style: move Bg / Main index => App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from './components/sections/Footer/Index';
 import { ThemeProvider, styled as MuiStyled } from '@mui/material';
 import createCustomTheme from './theme/palette';
 import { useColorMode } from './hooks/useColorMode';
+import Bg from './components/Bg';
 
 const Main = React.lazy(() => import('./components/sections/Main/Index'));
 const Skills = React.lazy(() => import('./components/sections/Skills/Index'));
@@ -25,6 +26,7 @@ function App() {
         <Project />
         <Contact />
         <Footer />
+        <Bg />
       </Container>
     </ThemeProvider>
   );
@@ -34,5 +36,6 @@ export default App;
 
 const Container = MuiStyled('div')({
   width: '100%',
-  height: '200vh',
+  height: '100vh',
+  position: 'relative',
 });

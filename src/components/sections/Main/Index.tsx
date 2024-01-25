5b2fe15ae0dbb0cd3745a5f115fc05c973aba845
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-25 15:13:50 +0900

    style: move Bg / Main index => App

--- src/components/sections/Main/Index.tsx
+++ src/components/sections/Main/Index.tsx
@@ -1,13 +1,10 @@
 import { styled as MuiStyled } from '@mui/material';
 import Turtle from './Turtle';
 import WaveTxt from './WaveTxt';
-import Ocean from './Ocean';
-import Bg from './Bg';
 
 export default function Main() {
   return (
     <Containter id="Home">
-      <Bg />
       <WaveTxt />
       <Turtle />
     </Containter>

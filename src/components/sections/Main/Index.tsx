3de7c2031f36e9a28ea98c320f7a8aec07cd924a
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-25 14:22:37 +0900

    chore: Bg components 분리

--- src/components/sections/Main/Index.tsx
+++ src/components/sections/Main/Index.tsx
@@ -2,11 +2,12 @@ import { styled as MuiStyled } from '@mui/material';
 import Turtle from './Turtle';
 import WaveTxt from './WaveTxt';
 import Ocean from './Ocean';
+import Bg from './Bg';
 
 export default function Main() {
   return (
     <Containter id="Home">
-      <Bg></Bg>
+      <Bg />
       <WaveTxt />
       <Turtle />
     </Containter>
@@ -20,19 +21,3 @@ const Containter = MuiStyled('main')({
   justifyContent: 'center',
   alignItems: 'center',
 });
-
-const Bg = MuiStyled('div')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  bottom: 0,
-  width: '100%',
-  height: '70%',
-  zIndex: -1,
-  opacity: 0.7,
-  transition: 'background-image 2s',
-  backgroundImage: 'linear-gradient(to bottom,#ffffff 0%,#ffecd2 20%, #fcb69f 100%)',
-
-  ...(theme.palette.mode === 'dark' && {
-    backgroundImage: 'linear-gradient(to bottom,#ffffff 0%, #0c6060 20%, #330867 100%)',
-  }),
-}));

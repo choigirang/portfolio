e70dad73ac4429a90528220e7eee728f9f16984f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-26 15:16:39 +0900

    feat: stack constant 에서 가져온 스택

--- src/components/sections/Skills/StackList.tsx
+++ src/components/sections/Skills/StackList.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { allStack } from '../../../constant/stack';
-import { AllStackType } from '../../../type/skillsType';
+import { Button, styled as MuiStyled } from '@mui/material';
 
 interface StackListProps {
-  stack: keyof AllStackType;
+  stack: string; // stack의 타입을 keyof AllStackType으로 지정
 }
 
-export default function StackList({ stack }): React.FC<StackListProps> {
+export default function StackList({ stack }: StackListProps) {
+  const [showStack, setShowStack] = useState(stack);
   const selectedStack = allStack[stack];
-  return <div></div>;
+
+  return (
+    <Container>
+      <IconBox>
+        {Object.entries(selectedStack).map(([key, value]) => (
+          <IconBtn>
+            <img src={`https://cdn.simpleicons.org/${key}`} alt="icon" width="50" height="50" />
+          </IconBtn>
+        ))}
+      </IconBox>
+    </Container>
+  );
 }
+
+const Container = MuiStyled('div')(({ theme }) => ({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 10,
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const IconBox = MuiStyled('div')(({ theme }) => ({
+  width: '100%',
+  display: 'flex',
+  gap: 10,
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const IconBtn = MuiStyled(Button)(({ theme }) => ({
+  borderRadius: '50%',
+  backgroundColor: 'white',
+  border: 'solid 3px #9c9c9c40',
+}));

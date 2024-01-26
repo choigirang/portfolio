3313a67530a8d86d7f59b02616f2e12a0c4ab71e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-26 19:46:41 +0900

    delete height

--- src/components/sections/Skills/Index.tsx
+++ src/components/sections/Skills/Index.tsx
@@ -59,7 +59,6 @@ export default function Skills() {
 
 const Container = MuiStyled('div')({
   width: '100%',
-  height: '100vh',
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',

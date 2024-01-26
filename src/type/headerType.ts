93f91f465a816be2341f926fc4f700b3ab87d7fd
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-26 18:52:00 +0900

    feat: add ListType & PropsType for change color

--- src/type/headerType.ts
+++ src/type/headerType.ts
@@ -1,4 +1,17 @@
 import { LinkProps } from 'react-router-dom';
+import { SvgIconComponent } from '@mui/icons-material';
+import { SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+
+export type ListType = {
+  [category: string]: {
+    [key: string]: string | SvgIconComponent;
+  };
+};
+
+export type ListChangeSpanColorProps = {
+  activeUrl: boolean;
+};
 
 export type HeaderScrollState = {
   $scroll: boolean;

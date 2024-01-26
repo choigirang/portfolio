ade82b255cc91bb9ad56eb944c4e4df35de15454
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-26 19:46:56 +0900

    style: add gird

--- src/components/sections/Main/Index.tsx
+++ src/components/sections/Main/Index.tsx
@@ -1,13 +1,22 @@
 import { styled as MuiStyled } from '@mui/material';
+import ProjectCard from './ProjectCard';
+import SkillsCard from './SkillsCard';
 
 export default function Main() {
-  return <Containter id="home"></Containter>;
+  return (
+    <Containter id="home">
+      <ProjectCard />
+      <SkillsCard />
+      <SkillsCard />
+    </Containter>
+  );
 }
 
 const Containter = MuiStyled('main')({
   width: '100%',
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+  display: 'grid',
+  gridTemplateColumns: 'minmax(400px, 3fr) 1fr 1fr',
+  gap: 20,
+  padding: '100px 5% 0 5%',
+  boxSizing: 'border-box',
 });

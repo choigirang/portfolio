42263228fa061b2ed6f1e1bef4086c51d8c43b0f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-26 18:50:47 +0900

    feat: left 고정, edit label

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { HeaderScrollState, TabPropsType } from '../type/headerType';
+import React, { ReactNode, useEffect, useState } from 'react';
+import { HeaderScrollState, ListChangeSpanColorProps, ListType, TabPropsType } from '../type/headerType';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Tabs, Tab, styled as MuiStyled, Typography, useTheme } from '@mui/material';
 import useMoveScroll from '../hooks/useMoveScroll';
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import { reverseAni, sunAni } from '../styles/keyframe';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import ArticleIcon from '@mui/icons-material/Article';
+import AutoAwesomeMosaicIcon from '@mui/icons-material/AutoAwesomeMosaic';
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import { SvgIconComponent } from '@mui/icons-material';
+
+// 주소 목록
+// const lists = ['home', 'about', 'skills', 'project', 'contact'];
+const listsLabel: { [key: string]: string } = {
+  home: '홈',
+  about: '소개',
+  skills: '기술',
+  project: '프로젝트',
+  contact: '연락망',
+};
+
+const listsIcon: { [key: string]: ReactNode } = {
+  홈: <HomeIcon />,
+  소개: <PersonIcon />,
+  기술: <ArticleIcon />,
+  프로젝트: <AutoAwesomeMosaicIcon />,
+  연락망: <ConnectWithoutContactIcon />,
+};
 
 // 주소 목록
 const lists = ['home', 'about', 'skills', 'project', 'contact'];
@@ -76,19 +97,6 @@ export default function Index() {
   return (
     <Header $scroll={scroll}>
       <Logo variant="h1" onClick={scrollToTop}>
-        {theme ? (
-          <WbSunnyIcon
-            sx={{
-              animation: `${sunAni} 2s linear infinite`,
-            }}
-          />
-        ) : (
-          <DarkModeIcon
-            sx={{
-              animation: `${reverseAni} 3s linear infinite`,
-            }}
-          />
-        )}
         Girang's
       </Logo>
       <Tabs
@@ -97,7 +105,12 @@ export default function Index() {
         {lists.map(list => (
           <TabList
             key={list}
-            label={list.replace('/', '')}
+            label={
+              <TabListWithIcons activeUrl={list === currentPath.pathname.replace('/', '')}>
+                {listsIcon[listsLabel[list]]}
+                <div className="labelKor">{listsLabel[list]}</div>
+              </TabListWithIcons>
+            }
             value={'/' + list}
             to={list}
             component={Link}
@@ -112,52 +125,98 @@ export default function Index() {
 const Header = MuiStyled('div')<HeaderScrollState>(({ theme, $scroll }) => ({
   display: 'flex',
   position: 'fixed',
+  flexDirection: 'column',
+  gap: 100,
   top: 0,
-  justifyContent: 'space-between',
   alignItems: 'center',
-  width: '100%',
-  height: '100px',
-  padding: '0 calc((100vw - 1280px) / 2)',
+  width: '240px',
+  height: '100vh',
+  padding: '38px 32px 32px 32px',
   color: 'white',
   boxSizing: 'border-box',
   transition: 'all 1s',
   zIndex: 1,
 
+  '& .MuiTab-wrapper': {
+    alignItems: 'self-start',
+    justifyContent: 'flex-start',
+  },
+
+  '& .MuiTabs-flexContainer': {
+    display: 'flex',
+    flexDirection: 'column !important',
+    gap: 20,
+  },
+
   '.visited': {
     fontWeight: '500',
-    color: theme.palette.primary.light,
+    color: theme.palette.common.white,
   },
 
   ...(theme.palette.mode === 'dark' && {
     backgroundColor: 'transparent',
 
     ':visited': {
-      color: theme.palette.primary.dark,
+      color: theme.palette.common.white,
     },
   }),
 }));
 
+const TabListWithIcons = MuiStyled('div')<ListChangeSpanColorProps>(({ activeUrl, theme }) => ({
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  gap: 20,
+
+  svg: {
+    width: 18,
+    height: 18,
+    padding: 9,
+    color: `${activeUrl ? 'white' : '#9DA2B9'}`,
+    backgroundColor: '#1A1B24',
+    borderRadius: 5,
+  },
+
+  '.labelKor': {
+    display: 'flex',
+    alignItems: 'center',
+    color: `${activeUrl ? 'white' : '#9DA2B9'}`,
+
+    '& :visited': {
+      color: 'white',
+    },
+  },
+}));
+
 const TabList = MuiStyled(Tab)<TabPropsType>(({ theme }) => ({
-  '.Mui-selected': {
-    color: theme.palette.text.secondary,
+  display: 'flex',
+  fontFamily: 'S-CoreDream-3Light',
+  fontWeight: 900,
+  fontSize: 18,
+
+  a: {
+    display: 'inline !important',
+  },
+
+  '& :visited': {
+    color: 'white',
   },
 
   ...(theme.palette.mode === 'dark' && {
     color: theme.palette.common.white,
     '.Mui-selected': {
-      color: `${theme.palette.primary.main} !important`,
+      color: `${theme.palette.common.white} !important`,
     },
   }),
 }));
 
 const Logo = MuiStyled(Typography)(({ theme }) => ({
   width: '150px',
-  height: '100%',
   display: 'flex',
   gap: 10,
   alignItems: 'center',
   justifyContent: 'center',
-  color: theme.palette.primary.main,
+  color: theme.palette.common.white,
 
   '&:hover': {
     cursor: 'pointer',

3c69063530987151d59bea308c3ac63518022ddd
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-28 23:24:30 +0900

    create: 소개 페이지 텍스트

---
+++ src/components/sections/About/TextContainer.tsx
@@ -0,0 +1,8 @@
+import { styled as MuiStyled } from '@mui/material';
+import React from 'react';
+
+export default function TextContainer() {
+  return <Container>TextContainer</Container>;
+}
+
+const Container = MuiStyled('div')(({}) => ({}));

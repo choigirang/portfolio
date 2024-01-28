f65bc66ce3c4ec83ed2aa654923a37a11e025f6b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-28 23:24:18 +0900

    create: 소개 페이지

--- src/components/sections/About/Index.tsx
+++ src/components/sections/About/Index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { styled } from 'styled-components';
+import { styled as MuiStyled } from 'styled-components';
 
 export default function About() {
-  return <Container id="about">About</Container>;
+  return (
+    <Container id="about">
+      <Title>
+        <p>🧑🏻‍💻</p>
+        <p>소개</p>
+      </Title>
+    </Container>
+  );
 }
 
-const Container = styled('div')({
+const Container = MuiStyled('div')({
   width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const Title = MuiStyled('div')({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'white',
+  fontSize: 32,
+
+  p: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontWeight: 700,
+  },
 });

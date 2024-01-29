7a795c3f4504228b803920231b6d5728167e7cac
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-29 23:48:08 +0900

    chore: stack data edit

--- src/constant/info.tsx
+++ src/constant/info.tsx
@@ -83,14 +83,45 @@ export const skillsList: { [key: string]: string } = {
 };
 
 export const allStack: AllStackType = {
-  frontend: {
-    react: '',
-    reactQuery: '',
+  프론트엔드: {
+    react: {
+      title: 'react',
+      name: '리액트',
+      description: '설명',
+      link: [''],
+      color: '#1CCEFF',
+    },
+    reactQuery: {
+      title: 'reactQuery',
+      name: '리액트 쿼리',
+      description: '설명',
+      link: [''],
+      color: '#FF475A',
+    },
   },
-  backend: {
-    node: '',
+  백엔드: {
+    mongodb: {
+      title: 'mongodb',
+      name: '몽고 디비',
+      description: '설명',
+      link: [''],
+      color: '#53B145',
+    },
   },
-  tools: {
-    discord: '',
+  툴: {
+    discord: {
+      title: 'discord',
+      name: '디스코드',
+      description: '설명',
+      link: [''],
+      color: '#5A6AFA',
+    },
+    figma: {
+      title: 'figma',
+      name: '피그마',
+      description: '설명',
+      link: [''],
+      color: '#F35328',
+    },
   },
 };

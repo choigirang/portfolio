11b4af8bb726b82d63183b564d12359444a9fa3c
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-29 15:51:52 +0900

    chore: 폴더 구조 변경 각 type => 취합

--- src/type/footerType.ts
+++
@@ -1,3 +0,0 @@
-export type MenuOpenType = {
-  $openMenu: boolean;
-};
--- src/type/skillsType.ts
+++
@@ -1,9 +0,0 @@
-export type AllStackType = {
-  [category: string]: {
-    [tech: string]: string;
-  };
-};
-
-export type SkillCardProps = {
-  [key: string]: string;
-};

d2bc24ace3723ddbe2863dbaf8528c238e1f59f0
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-29 23:47:25 +0900

    feat: stack data 를 활용한 stack list mapping

---
+++ src/components/sections/skills/Index.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import { Button, List, styled as MuiStyled, Palette } from '@mui/material';
+import styled from 'styled-components';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import StackList from './StackList';
+import { allStack, skillsList } from '../../../constant/info';
+import { motion } from 'framer-motion';
+import { AllStackType, SelectStackType } from '../../../type/sections';
+
+export default function Skills() {
+  const [category, setCategory] = useState('프론트엔드');
+  const [stack, setStack] = useState('react');
+  const [stackInfo, setStackInfo] = useState<SelectStackType>({
+    title: 'react',
+    name: '리액트',
+    description: '',
+    link: '',
+    color: 'blue',
+  });
+  const categoryList = ['프론트엔드', '백엔드', '툴'];
+
+  const listProps = { category, stackInfo, stack, setStack };
+
+  useEffect(() => {
+    const detailStack = allStack[category][stack];
+    console.log(detailStack);
+    setStackInfo(detailStack);
+  }, [stack]);
+
+  return (
+    <Container id="skills">
+      <Title>
+        <p>🗂️</p>
+        <h3>기술 목록</h3>
+      </Title>
+      <SubTitleContatiner>
+        {categoryList.map(key => (
+          <SubTitle key={key} $select={category === key} onClick={() => setCategory(key)}>
+            {key}
+          </SubTitle>
+        ))}
+      </SubTitleContatiner>
+      {categoryList.map(key => category === key && <StackList key={key} {...listProps} />)}
+    </Container>
+  );
+}
+
+const Container = MuiStyled('section')({
+  width: '100%',
+  marginTop: 50,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: 20,
+});
+
+const Title = MuiStyled('div')(({ theme }) => ({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: theme.palette.primary.main,
+  fontSize: 32,
+
+  p: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontWeight: 700,
+  },
+}));
+
+const SubTitleContatiner = MuiStyled(motion.div)(({ theme }) => ({
+  width: '100%',
+  display: 'grid',
+  gap: 20,
+  fontSize: 32,
+  gridTemplateColumns: 'repeat(3, 1fr)',
+}));
+
+const SubTitle = MuiStyled('p')<{ $select: boolean }>(({ $select, theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  color: `${$select ? theme.palette.secondary.main : 'white'}`,
+  cursor: 'pointer',
+  position: 'relative',
+}));

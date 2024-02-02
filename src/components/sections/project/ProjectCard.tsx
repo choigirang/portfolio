bf89c2888d167520c3a1eacbf782c2795e664247
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-03 02:06:11 +0900

    feat: project card 선택 시 front || back 분리

--- src/components/sections/project/ProjectCard.tsx
+++ src/components/sections/project/ProjectCard.tsx
@@ -1,26 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import useGetImg from '../../../hooks/useGetImg';
 import { Button, styled as MuiStyled } from '@mui/material';
 import AboutImg from './AboutImg';
 import { projects } from '../../../constant/info';
 import { ProjectDetailType } from '../../../type/sections';
 import AboutContent from './AboutContent';
+import { SetStatOption } from 'aws-sdk/clients/transfer';
+import DescriptionContent from './DescriptionContent';
 
-export default function ProjectCard({ name }: { name: string }) {
+export default function ProjectCard({
+  name,
+  cardNum,
+  flipCard,
+  setFlipCard,
+}: {
+  name: string;
+  cardNum: number;
+  flipCard: number | undefined;
+  setFlipCard: Dispatch<SetStateAction<number | undefined>>;
+}) {
   const [projectData, setProjectData] = useState<ProjectDetailType>();
 
   useEffect(() => {
     if (!name) return;
     setProjectData(projects[name]);
-  }, []);
+  }, [name]);
+
+  const selectFilp = cardNum === flipCard;
+
+  const handleFlipCard = (cardNum: number) => {
+    if (cardNum === flipCard) setFlipCard(undefined);
+    else setFlipCard(cardNum);
+  };
 
   return (
-    <Container>
+    <Container flipCard={selectFilp} onClick={() => handleFlipCard(cardNum)}>
       <Card>
         {projectData && (
           <React.Fragment>
-            <AboutImg name={projectData.name} />
-            <AboutContent {...projectData}></AboutContent>
+            <FrontContent selectFlip={selectFilp}>
+              <AboutImg name={projectData.name} />
+              <AboutContent {...projectData} />
+            </FrontContent>
+            <DescriptionContent selectFlip={selectFilp} description={projectData?.description} />
           </React.Fragment>
         )}
       </Card>
@@ -28,16 +50,37 @@ export default function ProjectCard({ name }: { name: string }) {
   );
 }
 
-const Container = MuiStyled('section')(({ theme }) => ({
+const Container = MuiStyled('section')<{ flipCard: boolean }>(({ flipCard, theme }) => ({
   width: '100%',
   backgroundColor: '#1A1B24',
   borderRadius: 10,
+  transition: 'all 1s',
+  transformStyle: 'preserve-3d',
+
+  ...(flipCard && {
+    transition: 'all 1s',
+    transform: 'rotateY(180deg)',
+  }),
 }));
 
 const Card = MuiStyled('div')(({ theme }) => ({
   width: '100%',
+}));
+
+const FrontContent = MuiStyled('div')<{ selectFlip: boolean }>(({ selectFlip, theme }) => ({
+  width: '100%',
+  height: '100%',
   padding: '5%',
   display: 'flex',
   flexDirection: 'column',
   gap: 70,
+  backfaceVisibility: 'hidden',
+  transition: 'all .3s',
+  transform: selectFlip ? 'rotateY(180deg)' : 'rotateY(0)', // 뒤집히는 효과
+
+  // selectFlip이 true일 때는 앞면이 보이지 않도록 처리
+  ...(selectFlip && {
+    visibility: 'hidden',
+    transition: 'all .3s',
+  }),
 }));

261149935c9896e702a13167e372ac5a0de03d6e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-04 00:49:58 +0900

    feat: stack에 대한 컴폰너트 분리 및 props setting

--- src/components/sections/skills/StackList.tsx
+++ src/components/sections/skills/StackList.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Button, styled as MuiStyled } from '@mui/material';
 import { allStack } from '../../../constant/info';
 import { AllStackType, SelectStackType, StackListProps } from '../../../type/sections';
+import IconBox from './IconBox';
+import StackInfo from './StackInfo';
 
 export default function StackList({ category, stackInfo, stack, setStack }: StackListProps) {
   const [categoryData, setCategoryData] = useState<{ [tech: string]: SelectStackType }>();
@@ -16,37 +18,13 @@ export default function StackList({ category, stackInfo, stack, setStack }: Stac
     setDetailStack(detailStack);
   }, [stack]);
 
+  const iconProps = { categoryData, stack, setStack };
+  const infoProps = { stackInfo, detailStack };
+
   return (
     <Container>
-      <IconBox>
-        {categoryData &&
-          Object.entries(categoryData).map(([key, value]) => (
-            <IconBtn
-              onClick={() => setStack(key)}
-              key={key}
-              $matchStack={stack === value.displayName}
-              $stackColor={value.color}>
-              <img
-                src={`https://cdn.simpleicons.org/${value.color !== 'black' ? key : key + '/' + value.color}`}
-                alt="icon"
-                width="50"
-                height="50"
-              />
-            </IconBtn>
-          ))}
-      </IconBox>
-      {detailStack && stackInfo && (
-        <StackInfo $color={stackInfo.color}>
-          <StackName $color={stackInfo.color}>
-            <img src={`https://cdn.simpleicons.org/${detailStack.name}/white`} alt="icon" width="100" height="100" />
-            <h3>{detailStack.name}</h3>
-          </StackName>
-          <StackDetail>
-            <div>{detailStack.description}</div>
-            <div>{detailStack.link}</div>
-          </StackDetail>
-        </StackInfo>
-      )}
+      <IconBox {...iconProps} />
+      <StackInfo {...infoProps} />
     </Container>
   );
 }
@@ -55,58 +33,11 @@ const Container = MuiStyled('div')(({ theme }) => ({
   width: '100%',
   display: 'flex',
   flexDirection: 'column',
-  gap: 10,
+  gap: 30,
   alignItems: 'center',
   justifyContent: 'center',
 
   '.title': {
     color: 'white',
   },
-}));
-
-const IconBox = MuiStyled('section')(({ theme }) => ({
-  width: '100%',
-  display: 'grid',
-  justifyContent: 'center',
-  gap: 10,
-  gridTemplateColumns: 'repeat(auto-fit, minmax(50px, 100px))',
-}));
-
-const IconBtn = MuiStyled(Button)<{ $matchStack?: boolean; $stackColor: string }>(
-  ({ $matchStack, $stackColor, theme }) => ({
-    backgroundColor: `${$matchStack ? 'white' : '#1A1B24'}`,
-  }),
-);
-
-const StackInfo = MuiStyled('div')<{ $color: string }>(({ $color, theme }) => ({
-  width: '100%',
-  display: 'grid',
-  padding: '5%',
-  backgroundColor: `${$color !== 'black' ? $color : '#1A1B24'}`,
-  color: 'white',
-  borderRadius: 10,
-  gridTemplateRows: '1fr',
-  gridTemplateColumns: 'minmax(100px, 200px) auto',
-}));
-
-const StackName = MuiStyled('div')<{ $color: string }>(({ $color, theme }) => ({
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  gap: 20,
-
-  h3: {
-    color: $color,
-    fontWeight: 600,
-    backgroundColor: 'white',
-    padding: '5px 20px',
-    borderRadius: 10,
-  },
-}));
-
-const StackDetail = MuiStyled('section')(({ theme }) => ({
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
 }));

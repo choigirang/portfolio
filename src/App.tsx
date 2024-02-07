eb551a981224e3258e7fbc47195b8b61ca233540
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-07 23:08:40 +0900

    feat: window size에 따른 processBar on off

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useMemo } from 'react';
+import React, { Suspense, useEffect, useMemo, useState } from 'react';
 import { motion, useScroll } from 'framer-motion';
 import Header from './components/layout/Header';
 import Footer from './components/layout/footer/Index';
@@ -15,9 +15,27 @@ const Project = React.lazy(() => import('./components/sections/project/Index'));
 const Contact = React.lazy(() => import('./components/sections/contact/Index'));
 
 function App() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+  });
   const { mode } = useColorMode();
   const theme = useMemo(() => createCustomTheme(mode), [mode]);
   const { scrollYProgress } = useScroll();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    console.log(windowSize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -27,7 +45,7 @@ function App() {
             sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
           />
         }>
-        <motion.div className="progress-bar" style={{ scaleX: scrollYProgress }} />
+        {windowSize.width >= 1024 && <ProcessBar className="progress-bar" style={{ scaleX: scrollYProgress }} />}
         <Container>
           <Header />
           <DividContainer>
@@ -51,12 +69,44 @@ const Container = MuiStyled('div')({
   backgroundColor: '#0F1015',
   display: 'flex',
   justifyContent: 'end',
+
+  /* 타블렛 */
+  '@media screen and (max-width:1023px)': {
+    width: '100%',
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'column',
+  },
+
+  /* 모바일 */
+  '@media screen and (max-width:767px)': {},
 });
+
+const ProcessBar = MuiStyled(motion.div)(({ theme }) => ({
+  /* 타블렛 */
+  '@media screen and (max-width:1023px)': {
+    display: 'none',
+  },
+
+  /* 모바일 */
+  '@media screen and (max-width:767px)': {},
+}));
 
 const DividContainer = MuiStyled('div')(({ theme }) => ({
   width: 'calc(100% - 240px)',
+  position: 'relative',
   display: 'flex',
   flexDirection: 'column',
   padding: '0 5%',
   gap: 50,
+
+  /* 타블렛 */
+  '@media screen and (max-width:1023px)': {
+    width: '100%',
+  },
+
+  /* 모바일 */
+  '@media screen and (max-width:767px)': {
+    gap: 0,
+  },
 }));

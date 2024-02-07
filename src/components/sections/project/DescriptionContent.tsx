988150dbe5a44da7f922383bf5cf75066c01782b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-07 23:12:57 +0900

    style: width delete

--- src/components/sections/project/DescriptionContent.tsx
+++ src/components/sections/project/DescriptionContent.tsx
@@ -30,7 +30,6 @@ export default function DescriptionContent(props: { flipCard: boolean; descripti
 }
 
 const BackContent = MuiStyled('div')<{ flipCard: boolean }>(({ flipCard, theme }) => ({
-  width: '100%',
   height: '100%',
   position: 'absolute',
   top: 0,

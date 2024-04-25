153629076b650884ae91e28b2af5622718c4404b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-25 23:35:42 +0900

    refactor: components 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React, { Suspense, useEffect, useMemo, useState } from 'react';
-import Header from './components/layout/Header';
-import Main from './components/sections/main/Index';
+import Header from './components/common/Header/Header';
 import Footer from './components/layout/footer/Index';
-
-import { useColorMode } from './hooks/useColorMode';
-
-import { ThemeProvider, styled as MuiStyled } from '@mui/material';
-import { motion, useScroll } from 'framer-motion';
-import CircularProgress from '@mui/material/CircularProgress';
-import createCustomTheme from './theme/palette';
+import React from 'react';
+import Summary from './components/sections/Summary/Index';
 
 const About = React.lazy(() => import('./components/sections/about/Index'));
 const Skills = React.lazy(() => import('./components/sections/skills/Index'));
@@ -16,97 +9,19 @@ const Project = React.lazy(() => import('./components/sections/project/Index'));
 const Contact = React.lazy(() => import('./components/sections/contact/Index'));
 
 function App() {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-  });
-  const { mode } = useColorMode();
-  const theme = useMemo(() => createCustomTheme(mode), [mode]);
-  const { scrollYProgress } = useScroll();
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-      });
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [windowSize]);
-
   return (
-    <ThemeProvider theme={theme}>
-      <Suspense
-        fallback={
-          <CircularProgress
-            sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
-          />
-        }>
-        {windowSize.width >= 1024 && <ProcessBar className="progress-bar" style={{ scaleX: scrollYProgress }} />}
-        <Container>
-          <Header />
-          <DividContainer>
-            <Main />
-            <About />
-            <Skills />
-            <Project />
-            <Contact />
-          </DividContainer>
-          <Footer />
-        </Container>
-      </Suspense>
-    </ThemeProvider>
+    <div className="flex bg-[#0F1015]">
+      <Header />
+      <div className="relative flex flex-col p-10 gap-10 width-layout">
+        <Summary />
+        {/* <About />
+        <Skills />
+        <Project />
+        <Contact /> */}
+      </div>
+      {/* <Footer /> */}
+    </div>
   );
 }
 
 export default App;
-
-const Container = MuiStyled('div')({
-  width: '100%',
-  backgroundColor: '#0F1015',
-  display: 'flex',
-  justifyContent: 'end',
-
-  /* 타블렛 */
-  '@media screen and (max-width:1023px)': {
-    width: '100%',
-    display: 'flex',
-    position: 'relative',
-    flexDirection: 'column',
-  },
-
-  /* 모바일 */
-  '@media screen and (max-width:767px)': {},
-});
-
-const ProcessBar = MuiStyled(motion.div)(({ theme }) => ({
-  /* 타블렛 */
-  '@media screen and (max-width:1023px)': {
-    display: 'none',
-  },
-
-  /* 모바일 */
-  '@media screen and (max-width:767px)': {},
-}));
-
-const DividContainer = MuiStyled('div')(({ theme }) => ({
-  width: 'calc(100% - 240px)',
-  position: 'relative',
-  display: 'flex',
-  flexDirection: 'column',
-  padding: '0 5%',
-  gap: 50,
-
-  /* 타블렛 */
-  '@media screen and (max-width:1023px)': {
-    width: '100%',
-  },
-
-  /* 모바일 */
-  '@media screen and (max-width:767px)': {
-    gap: 0,
-  },
-}));

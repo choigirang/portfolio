4feb63d651cfb8488f5394e928fd0291d8cb89fa
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-25 23:36:13 +0900

    refactor: category components 분리

---
+++ src/components/common/Header/Category.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import useMoveScroll from '../../../hooks/useMoveScroll';
+import { headerLabel, headerListsIcon } from '../../../constant/info';
+
+export default function Category({ list }: { list: string }) {
+  const { scrollToPage } = useMoveScroll();
+
+  return (
+    <li
+      className="flex items-center gap-5 transition-custom text-stone-600 hover:text-yellow-400 cursor-pointer"
+      onClick={() => scrollToPage(list)}>
+      <span className="bg-stone-800 p-1 rounded-lg">{headerListsIcon[headerLabel[list]]}</span>
+      <span>{headerLabel[list]}</span>
+    </li>
+  );
+}

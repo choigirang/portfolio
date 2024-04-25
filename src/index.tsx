2d74322fe00edd716dc88764312b489d65d03044
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-25 23:39:09 +0900

    refactor: mui => tailwind,  delete provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,16 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import { ColorModeProvider } from './hooks/useColorMode';
-
 import './styles/reset.css';
 import './styles/global.css';
-import { StyledEngineProvider } from '@mui/styled-engine';
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <QueryClientProvider client={queryClient}>
-    <ColorModeProvider>
-      <StyledEngineProvider injectFirst>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </StyledEngineProvider>
-    </ColorModeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </QueryClientProvider>,
 );
 // If you want to start measuring performance in your app, pass a function

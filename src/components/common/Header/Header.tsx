69e00fa0c1ad86e60476e814d57d94aefd92e187
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-26 20:51:14 +0900

    chore: add key & styling

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -3,12 +3,12 @@ import Category from './Category';
 
 export default function Header() {
   return (
-    <header className="flex flex-col justify-start gap-12 w-[240px] h-screen p-10 text-white transition-custom">
+    <header className="fixed z-100 top-0 left-0 flex flex-col justify-start gap-12 w-[240px] h-screen p-10 text-white transition-custom">
       <h1 className="text-2xl font-bold">Girang's</h1>
       <nav>
         <ul className="flex flex-col gap-10">
           {headerLists.map(list => (
-            <Category list={list} />
+            <Category key={list} list={list} />
           ))}
         </ul>
       </nav>

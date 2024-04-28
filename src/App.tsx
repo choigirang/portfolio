4463de0dafb0f13a8bf213d5c07daae7f569e5e2
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-28 22:52:55 +0900

    refactor: clean up components & using Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import Header from './components/common/Header/Header';
-import Footer from './components/layout/footer/Index';
-import React from 'react';
-import Summary from './components/sections/Summary/Index';
+import React, { Suspense } from 'react';
 
 const About = React.lazy(() => import('./components/sections/About/Index'));
 const Skills = React.lazy(() => import('./components/sections/Skills/Index'));
-const Project = React.lazy(() => import('./components/sections/project/Index'));
+const Project = React.lazy(() => import('./components/sections/Project/Index'));
 
 function App() {
   return (
     <>
       <Header />
       <div className="flex flex-col p-10 pl-[240px] gap-10 width-layout">
-        <Summary />
-        <About />
-        <Skills />
-        {/* <Project />*/}
+        <Suspense fallback={<div>Loading...</div>}>
+          <About />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Skills />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Project />
+        </Suspense>
       </div>
-      {/* <Footer /> */}
     </>
   );
 }

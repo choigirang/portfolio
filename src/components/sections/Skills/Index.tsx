4cbbe4fde893dcdb6b1673e5f0337c8a3db10c7f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-28 22:58:56 +0900

    refactor: index 에서 관리하던 state 분리

--- src/components/sections/Skills/Index.tsx
+++ src/components/sections/Skills/Index.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import StackList from './StackList';
+import { allStack } from '../../../constant/info';
+import StackDetail from './StackDetail';
+import { SelectStackType } from '../../../type/sections';
 
 /**
  *
@@ -9,6 +12,16 @@ const categoryList = ['언어', '프론트엔드', '백엔드', '툴'];
 
 export default function Skills() {
   const [category, setCategory] = useState('언어');
+  const [stack, setStack] = useState('');
+  const [data, setData] = useState<SelectStackType | undefined>();
+
+  useEffect(() => {
+    setStack(Object.keys(allStack[category])[0]);
+  }, [category]);
+
+  useEffect(() => {
+    setData(allStack[category][stack]);
+  }, [stack]);
 
   return (
     <section id="skills" className="flex flex-col justify-center items-center gap-5 w-full">
@@ -27,7 +40,8 @@ export default function Skills() {
           </li>
         ))}
       </ul>
-      <StackList select={category} />
+      <StackList select={category} stack={stack} setStack={setStack} />
+      <StackDetail data={data} />
     </section>
   );
 }

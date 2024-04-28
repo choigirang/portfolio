1f34ba48ec2b9a7d22dd970ced109ef74f0843ce
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-28 22:59:21 +0900

    refactor: 상위 컴포넌트에서 분리 처리된 data props

--- src/components/sections/Skills/StackDetail.tsx
+++ src/components/sections/Skills/StackDetail.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { SelectStackType } from '../../../type/sections';
 import { url } from './StackList';
+import { SelectStackType } from '../../../type/sections';
 
-export default function StackDetail(data: SelectStackType) {
+export default function StackDetail({ data }: { data: SelectStackType | undefined }) {
   return (
-    <li>
-      <img src={url + data.name} alt="stack-img" className="w-[50px] h-[50px]" />
-      <ul className="flex flex-col"></ul>
-    </li>
+    <ul className="w-full h-[200px] grid grid-cols-stackDetail bg-[#1A1B24] p-5 rounded-lg">
+      {data ? (
+        <React.Fragment>
+          <li className="flex justify-center items-center w-full h-full">
+            <img src={url + data.name} alt="stack-img" className="w-[100px] h-[100px] p-3 bg-[#30313d] rounded-lg" />
+          </li>
+          <ul className="flex flex-col gap-2 w-full h-full overflow-scroll text-white">
+            {data.description.map(each => (
+              <li key={each}>• {each}</li>
+            ))}
+          </ul>
+        </React.Fragment>
+      ) : (
+        <li>데이터를 불러오고 있습니다.</li>
+      )}
+    </ul>
   );
 }
